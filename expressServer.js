4713d4a92b817ace8d9b45735c41fc8b5a2c5100
Muniba Waqar <muniba.wq@gmail.com>
2021-01-05 16:40:18 -0700

    first commit

---
+++ expressServer.js
@@ -0,0 +1,3 @@
+setInterval(() => {
+  console.log(".5 second");
+}, 1000);
---
+++ nodeServer.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+//the http moduale has a createServer method
+
+//takes 1 arg:
+// 1. callback, callback, has 2 args: req, res (a request and a reponse object)
+
+//the server variable:
+//
+const server = http.createServer((req, res) => {});
