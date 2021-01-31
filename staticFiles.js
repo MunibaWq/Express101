e59fd190486ae7d654ef5bfc1cb2ff8550f613f0
Muniba Waqar <muniba.wq@gmail.com>
2021-01-31 14:16:52 -0700

    Serving Static Files in Express - accessing the 'public' directory using the static method

---
+++ staticFiles.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+//app comes with a 'USE' method
+// use takes 1 arg (right now)
+// 1. the middleware you want to run
+//public is the directory that we want and is handed to the static module, we dont need to include 'public' in the path
+//basically we are saying
+// express - anything in this 'public' directory please serve it up, we dont want to mess with it
+app.use(express.static("public"));
+
+app.listen(3000);
+console.log(`The server is listening on port 3000`);
