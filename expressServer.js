1a83119167206b8673c9cdf9211a54d26077790a
Muniba Waqar <muniba.wq@gmail.com>
2021-01-31 13:34:21 -0700

    create a basic express server

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,7 @@
 //NODEJS is the language
 // Express is node, a node module
+//npm init to add a package.json file
+//'npm install express' to add express dependency
 
 //http is a native module
 const http = require("http");
@@ -8,7 +10,20 @@ const http = require("http");
 //that means that we will need to run 'npm install'
 const express = require("express");
 
-//npm init to add a package.json file
-//'npm install express --save' to add express dependency
+//An 'app' is the express function (creatApplication inside the Express module)
+//invoked and is an express applicaiton
+const app = express();
 
-const app = express(); //whatever was exported by the express module is stored in the 'app' variable created here
+//All is a method, and it takes 2 args:
+// 1. route
+// 2. callback to run if the route is requested
+
+//the '*' means we'll accept anything, we're not making use of the router, that way its a mirror image of our very first node server
+app.all("*", (req, res) => {
+  // Express handles the basic header (status code, mime-type)!
+  res.send(`<h1>This is the homepage</h1>`); //this is the in-between we have to deal with
+  //Express handles the end!
+});
+
+app.listen(3000); //specify a port
+console.log(`the server is listening on port 3000`);
