cef479f1fbc546e3c2f2bbb82822fca5c5494c06
Muniba Waqar <muniba.wq@gmail.com>
2021-01-31 13:19:36 -0700

    installing express, creating a package.json file and gitignore file

--- expressServer.js
+++ expressServer.js
@@ -1,3 +1,14 @@
-setInterval(() => {
-  console.log(".5 second");
-}, 1000);
+//NODEJS is the language
+// Express is node, a node module
+
+//http is a native module
+const http = require("http");
+
+// express is a 3rd party module
+//that means that we will need to run 'npm install'
+const express = require("express");
+
+//npm init to add a package.json file
+//'npm install express --save' to add express dependency
+
+const app = express(); //whatever was exported by the express module is stored in the 'app' variable created here
