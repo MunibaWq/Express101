d5640b6d0d9efa3fa94d8ca6c5f82b6c1226cf6c
Muniba Waqar <muniba.wq@gmail.com>
2021-01-31 15:29:07 -0700

    Serving Static Files in Express - adding a route to a static file in expressServer.js, also created a basic 404

--- expressServer.js
+++ expressServer.js
@@ -3,6 +3,8 @@
 //npm init to add a package.json file
 //'npm install express' to add express dependency
 
+//path is a native module to nodejs
+const path = require("path");
 //http is a native module
 const http = require("http");
 
@@ -14,15 +16,28 @@ const express = require("express");
 //invoked and is an express applicaiton
 const app = express();
 
+// serve up static files! Only 1 line... take that nodejs
+app.use(express.static("public"));
+
 //All is a method, and it takes 2 args:
 // 1. route
 // 2. callback to run if the route is requested
 
 //the '*' means we'll accept anything, we're not making use of the router, that way its a mirror image of our very first node server
-app.all("*", (req, res) => {
+// app.all("*", (req, res) => {
+app.all("/", (req, res) => {
   // Express handles the basic header (status code, mime-type)!
-  res.send(`<h1>This is the homepage</h1>`); //this is the in-between we have to deal with
+  // read in Node.html
+  console.log(path.join(__dirname + "/node.html"));
+  res.sendFile(path.join(__dirname + "/node.html"));
+
+  // send this file if an HTTP request of any type is made to '/' from app.all('/',...)
+  // res.send(`<h1>This is the homepage</h1>`); //this is the in-between we have to deal with
   //Express handles the end!
+});
+
+app.all("*", (req, res) => {
+  res.send("<h1>Sorry, this page does not exist</h1>");
 });
 
 app.listen(3000); //specify a port
--- staticFiles.js
+++ staticFiles.js
@@ -7,6 +7,8 @@ const app = express();
 //public is the directory that we want and is handed to the static module, we dont need to include 'public' in the path
 //basically we are saying
 // express - anything in this 'public' directory please serve it up, we dont want to mess with it
+//some people use this to serve an entire frontend side
+// all you need to type in is localhost:3000/node.png or localhost:3000/styles.css you dont need a path to public
 app.use(express.static("public"));
 
 app.listen(3000);
